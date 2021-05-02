1c3ce3d37c024ff1f1cc7f3ae3fe0ac169259923
Mika Yeap <yeapmika@gmail.com>
2021-05-02 13:56:15 +0800

    prod run files

---
+++ run-prod.sh
@@ -0,0 +1,11 @@
+export PORT=8001
+
+export REDISHOST=34.126.72.14
+
+export REDISPORT=6379
+
+export REDISPASS=2021myikaredis@ccount99853422701
+
+go build -o build/api .
+
+build/api
--- run.sh
+++ run.sh
@@ -1,11 +1,5 @@
 export PORT=8001
 
-export REDISHOST=34.126.72.14
+go build -o build/api .
 
-export REDISPORT=6379
-
-export REDISPASS=2021myikaredis@ccount99853422701
-
-go build -o build/strat .
-
-build/strat
+build/api
