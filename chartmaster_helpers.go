8b15e513d941f88fa85febb73a22057a84e890f1
SImon <simon20000808@gmail.com>
2021-06-23 23:14:36 +0900

    uncomment fetch func

--- chartmaster_helpers.go
+++ chartmaster_helpers.go
@@ -8,6 +8,7 @@ import (
 	"io/ioutil"
 	"math"
 	"math/rand"
+	"net/http"
 	"os"
 	"runtime"
 	"sort"
@@ -55,54 +56,53 @@ func cacheCandleData(candles []Candlestick, ticker, period string) {
 }
 
 func fetchCandleData(ticker, period string, start, end time.Time) []Candlestick {
-	// fetchEndTime := end.Add(1 * periodDurationMap[period])
-	// _, file, line, _ := runtime.Caller(0)
-	// go Log(fmt.Sprintf("FETCHING new candles %v -> %v", start.Format(httpTimeFormat), fetchEndTime.Format(httpTimeFormat)),
-	// 	fmt.Sprintf("<%v> %v", line, file))
+	fetchEndTime := end.Add(1 * periodDurationMap[period])
+	_, file, line, _ := runtime.Caller(0)
+	go Log(fmt.Sprintf("FETCHING new candles %v -> %v", start.Format(httpTimeFormat), fetchEndTime.Format(httpTimeFormat)),
+		fmt.Sprintf("<%v> %v", line, file))
 
-	// //send request
-	// base := "https://rest.coinapi.io/v1/ohlcv/BINANCEFTS_PERP_BTC_USDT/history" //TODO: build dynamically based on ticker
-	// full := fmt.Sprintf("%s?period_id=%s&time_start=%s&time_end=%s",
-	// 	base,
-	// 	period,
-	// 	start.Format(httpTimeFormat),
-	// 	fetchEndTime.Format(httpTimeFormat))
+	//send request
+	base := "https://rest.coinapi.io/v1/ohlcv/BINANCEFTS_PERP_BTC_USDT/history" //TODO: build dynamically based on ticker
+	full := fmt.Sprintf("%s?period_id=%s&time_start=%s&time_end=%s",
+		base,
+		period,
+		start.Format(httpTimeFormat),
+		fetchEndTime.Format(httpTimeFormat))
 
-	// req, _ := http.NewRequest("GET", full, nil)
-	// req.Header.Add("X-CoinAPI-Key", "170F2DBA-F62F-4649-857C-2A2A5A6C62A1")
-	// client := &http.Client{}
-	// response, err := client.Do(req)
+	req, _ := http.NewRequest("GET", full, nil)
+	req.Header.Add("X-CoinAPI-Key", "170F2DBA-F62F-4649-857C-2A2A5A6C62A1")
+	client := &http.Client{}
+	response, err := client.Do(req)
 
-	// if err != nil {
-	// 	_, file, line, _ := runtime.Caller(0)
-	// 	go Log(fmt.Sprintf("GET candle data err %v\n", err), fmt.Sprintf("<%v> %v", line, file))
-	// 	return nil
-	// }
+	if err != nil {
+		_, file, line, _ := runtime.Caller(0)
+		go Log(fmt.Sprintf("GET candle data err %v\n", err), fmt.Sprintf("<%v> %v", line, file))
+		return nil
+	}
 
-	// //parse data
-	// body, _ := ioutil.ReadAll(response.Body)
-	// // fmt.Println(string(body))
-	// var jStruct []Candlestick
-	// errJson := json.Unmarshal(body, &jStruct)
-	// if errJson != nil {
-	// 	_, file, line, _ := runtime.Caller(0)
-	// 	go Log(fmt.Sprintf("JSON unmarshall candle data err %v\n", errJson),
-	// 		fmt.Sprintf("<%v> %v", line, file))
-	// }
-	// //save data to cache so don't have to fetch again
-	// if len(jStruct) > 0 && jStruct[0].Open != 0 {
-	// 	go cacheCandleData(jStruct, ticker, period)
+	//parse data
+	body, _ := ioutil.ReadAll(response.Body)
+	// fmt.Println(string(body))
+	var jStruct []Candlestick
+	errJson := json.Unmarshal(body, &jStruct)
+	if errJson != nil {
+		_, file, line, _ := runtime.Caller(0)
+		go Log(fmt.Sprintf("JSON unmarshall candle data err %v\n", errJson),
+			fmt.Sprintf("<%v> %v", line, file))
+	}
+	//save data to cache so don't have to fetch again
+	if len(jStruct) > 0 && jStruct[0].Open != 0 {
+		go cacheCandleData(jStruct, ticker, period)
 
-	// 	//temp save to loval file to preserve CoinAPI credits
-	// 	fileName := fmt.Sprintf("%v,%v,%v,%v|%v.json", ticker, period, start, end, time.Now().Unix())
-	// 	file, _ := json.MarshalIndent(jStruct, "", " ")
-	// 	_ = ioutil.WriteFile(fileName, file, 0644)
-	// } else {
-	// 	_, file, line, _ := runtime.Caller(0)
-	// 	go Log(fmt.Sprint(string(body)), fmt.Sprintf("<%v> %v", line, file))
-	// }
-	// return jStruct
-	return nil
+		//temp save to loval file to preserve CoinAPI credits
+		fileName := fmt.Sprintf("%v,%v,%v,%v|%v.json", ticker, period, start, end, time.Now().Unix())
+		file, _ := json.MarshalIndent(jStruct, "", " ")
+		_ = ioutil.WriteFile(fileName, file, 0644)
+	} else {
+		_, file, line, _ := runtime.Caller(0)
+		go Log(fmt.Sprint(string(body)), fmt.Sprintf("<%v> %v", line, file))
+	}
+	return jStruct
 }
 
 func getCachedCandleData(ticker, period string, start, end time.Time) []Candlestick {
@@ -458,7 +458,7 @@ func computeBacktest(
 		}
 
 		//run strat for all chunk's candles
-		for i, candle := range allCandlesArr {
+		for _, candle := range allCandlesArr {
 			var chunkAddedCandles []CandlestickChartData //separate chunk added vars to stream new data in packet only
 			var chunkAddedPCData []ProfitCurveDataPoint
 			var chunkAddedSTData []SimulatedTradeDataPoint
